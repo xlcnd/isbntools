96427cd8b92b57c838452d5b52a84b00424a27c5
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-01 21:06:44 +0000

    uses urllib2 instead of httplib2

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,6 +1,8 @@
-__all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta', 'to_isbn10',
-           'to_isbn13', 'get_isbnlike', 'notisbn', 'canonical', 'get_canonical_isbn']
+__all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
+           'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
+           'canonical', 'get_canonical_isbn']
+
 __version__ = '0.8.1'
 
-from .isbntoolscore import is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean, canonical, notisbn, get_isbnlike, get_canonical_isbn, mask, meta, info
-
+from .isbntoolscore import is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,\
+    canonical, notisbn, get_isbnlike, get_canonical_isbn, mask, meta, info
