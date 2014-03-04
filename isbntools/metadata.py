b923ed9eb2141d774e32d5bba11375bd2b83515d
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-04 18:57:26 +0000

    metadata services with regestry pattern

--- isbntools/metadata.py
+++ isbntools/metadata.py
@@ -1,19 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import wcat
-import googlebooks
+from regestry import services
 
 
-def query(isbn, service='wcat'):
+def query(isbn, service='default'):
     """
     Queries worldcat.org or Google Books (JSON API) for metadata
     """
-    if service == 'wcat':
-        canonical = wcat.query(isbn)
-    elif service == 'goob' or service == 'googlebooks':
-        canonical = googlebooks.query(isbn)
-    else:
+    if service not in services:
         print(('Error:%s is not a recognized service!' % service))
         return
-    return canonical
+    return services[service](isbn)
