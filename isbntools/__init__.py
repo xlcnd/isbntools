3cbb13a61fe8fdb3c122e9fa0908baa15b9ec99b
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-06 08:36:58 +0000

    init v1.0.3

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -2,7 +2,7 @@ __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
            'canonical', 'get_canonical_isbn', 'webservice', 'editions']
 
-__version__ = '1.0.2'
+__version__ = '1.0.3'
 
 import webservice
 from .isbntoolscore import is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,\
