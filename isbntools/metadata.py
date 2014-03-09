290a56105e13dd60b01e8419843b4a804e970ff3
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-09 11:59:49 +0000

    set default timeout for all sockets (at right level)

--- isbntools/metadata.py
+++ isbntools/metadata.py
@@ -1,8 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import socket 
 from .registry import services
 
+# socket timeout is not exposed at urllib2 level so I had to import the 
+# module and set a default value for all the sockets (timeout in seconds)
+# however this should be done at top level due to strong side effects...
+socket.setdefaulttimeout(10)
 
 def query(isbn, service='default'):
     """
