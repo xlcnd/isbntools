5d95dc5a31e1c7bbcbc0685a147a25c264a7f1a7
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-09 11:00:06 +0000

    set default timeout for all sockets

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -4,6 +4,6 @@ __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
 
 __version__ = '2.0.2'
 
-from .isbntoolscore import is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,\
-    canonical, notisbn, get_isbnlike, get_canonical_isbn, mask, meta, info,\
-    editions
+from .isbntoolscore import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
+    canonical, notisbn, get_isbnlike, get_canonical_isbn, mask, meta, info,
+    editions)
