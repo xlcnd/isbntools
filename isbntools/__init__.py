0cae2ad57fdb721b955542d20a3ced2e04bf34b8
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-10 11:14:13 +0000

    init v2.0.3

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -2,7 +2,7 @@ __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
            'canonical', 'get_canonical_isbn', 'editions']
 
-__version__ = '2.0.2'
+__version__ = '2.0.3'
 
 from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
     canonical, notisbn, get_isbnlike, get_canonical_isbn)
