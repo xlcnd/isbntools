bb12016038e0a79c3a5d2fb869884e1e43556919
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-10 13:52:01 +0000

    quiet_errors for scripts

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,9 +1,10 @@
 __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
-           'canonical', 'get_canonical_isbn', 'editions']
+           'canonical', 'get_canonical_isbn', 'editions', 'quiet_errors']
 
 __version__ = '2.0.3'
 
+from .exceptions import quiet_errors
 from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
     canonical, notisbn, get_isbnlike, get_canonical_isbn)
 from .ext import (mask, meta, info, editions)
