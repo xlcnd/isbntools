0b13b69ea3e6c60cf881186b67d778446dc908ea
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-11 18:54:26 +0000

    NotValidISBNError

--- isbntools/test/test_info.py
+++ isbntools/test/test_info.py
@@ -3,18 +3,18 @@
 
 
 from ..infogroup import infogroup
-from nose.tools import assert_equals
+from nose.tools import assert_equals, assert_raises
 
 
 # nose tests
 def test_infogroup():
     assert_equals(infogroup('9789720404427'), 'Portugal')
-    assert_equals(infogroup('720404427'), 'China')
+    assert_equals(infogroup('7204044271'), 'China')
     assert_equals(infogroup('9524712946'), 'Finland')
     assert_equals(infogroup('0330284983'),
                   'English - (UK, US, Australia, NZ, Canada,South Africa, Zimbabwe) [Ireland, Puerto Rico, Swaziland]')
     assert_equals(infogroup('3796519008'),
                   'German (Germany, Austria, Switzerland)')
     assert_equals(infogroup('92xxxxxxxxxxx'), None)
-    assert_equals(infogroup(''), None)
+    assert_raises(Exception, infogroup, '')
     assert_equals(infogroup('9791090636071'), 'France')
