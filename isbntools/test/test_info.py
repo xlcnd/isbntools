c58a8c8af9eb3625ad3e75943b24637c089b2dcc
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-12 23:26:08 +0000

    more efficient data structure in data4info.py

--- isbntools/test/test_info.py
+++ isbntools/test/test_info.py
@@ -13,7 +13,7 @@ def test_infogroup():
     assert_equals(infogroup('7204044271'), 'China')
     assert_equals(infogroup('9524712946'), 'Finland')
     assert_equals(infogroup('0330284983'),
-                  'English - (UK, US, Australia, NZ, Canada,South Africa, Zimbabwe) [Ireland, Puerto Rico, Swaziland]')
+                  'English - (UK, US, Australia, NZ, Canada,South Africa, Zimbabwe) (Ireland, Puerto Rico, Swaziland)')
     assert_equals(infogroup('3796519008'),
                   'German (Germany, Austria, Switzerland)')
     assert_equals(infogroup('92xxxxxxxxxxx'), None)
