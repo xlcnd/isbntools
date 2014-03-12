2d60c765eec6b119ddd813aa994dfabea67f519f
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-12 14:53:01 +0000

    improved coverage

--- isbntools/test/test_core.py
+++ isbntools/test/test_core.py
@@ -11,25 +11,33 @@ from ..data.data4tests import ISBNs
 
 # nose tests
 
-# def test__check_digit10(firstninedigits):
-#     pass
+def test__check_digit10():
+    assert_equals(_check_digit10('082649752'), '7')
+    assert_equals(_check_digit10('08264975X'), None)
+    assert_equals(_check_digit10('08264975'), None)
 
 
-# def test__check_digit13(firsttwelvedigits):
-#     return
+def test__check_digit13():
+    assert_equals(_check_digit13('978082649752'), '9')
+    assert_equals(_check_digit13('97808264975'), None)
+    assert_equals(_check_digit13('97808264975X'), None)
 
 
-# def test__check_structure10(isbn10like):
-#     return
+def test__check_structure10():
+    assert_equals(_check_structure10('0826497527'), True)
+    assert_equals(_check_structure10('0826497X27'), True) # isbnlike!
+    assert_equals(_check_structure10('0826497XI7'), False)
 
 
-# def test__check_structure13(isbn13like):
-#     return
+def test__check_structure13():
+    assert_equals(_check_structure13('9780826497529'), True)
+    assert_equals(_check_structure13('978082649752X'), False)
 
 
 def test_is_isbn10():
     assert_equals(is_isbn10('0826497527'), True)
     assert_equals(is_isbn10('0826497520'), False)
+    assert_equals(is_isbn10('954430603X'), True)
 
 
 def test_is_isbn13():
@@ -41,11 +49,14 @@ def test_is_isbn13():
 def test_to_isbn10():
     assert_equals(to_isbn10('9780826497529'), '0826497527')
     assert_equals(to_isbn10('9780826497520'), '0826497527')  # ISBN13 not valid
+    assert_equals(to_isbn10('9790826497529'), None)
+    assert_equals(to_isbn10('97808264975X3'), None)
 
 
 def test_to_isbn13():
     assert_equals(to_isbn13('0826497527'), '9780826497529')
     assert_equals(to_isbn13('0826497520'), '9780826497529')  # ISBN10 not valid
+    assert_equals(to_isbn13('08X6497527'), None)
 
 
 def test_clean():
@@ -62,6 +73,7 @@ def test_notisbn():
     assert_equals(notisbn('978082649752', level='loose'), True)
     assert_equals(notisbn('9780826400001', level='loose'), False)
     assert_equals(notisbn('9780826400001', level='strict'), True)
+    assert_equals(notisbn('9780826400001', level='badlevel'), None)
     assert_equals(notisbn('978 9426497529'), True)
     assert_equals(notisbn('9789426497529'), True)
     assert_equals(notisbn('979 10 9063607 1'), False)
@@ -76,10 +88,6 @@ def test_get_isbnlike():
     assert_equals(get_isbnlike(ISBNs, 'e'), None)
 
 
-# def test_isbn_stdin_validate():
-#     pass
-
-
 def test_get_canonical_isbn():
     assert_equals(get_canonical_isbn('0826497527', output='bouth'),
                   '0826497527')
@@ -90,6 +98,8 @@ def test_get_canonical_isbn():
                   '9780826497529')
     assert_equals(get_canonical_isbn('ISBN 0826497527', output='isbn13'),
                   '9780826497529')
+    assert_equals(get_canonical_isbn('ISBN 0826497527', output='NOOPTION'),
+                  None)                  
     assert_equals(get_canonical_isbn('0826497520'), None)
     assert_equals(get_canonical_isbn('9780826497529'), '9780826497529')
     assert_equals(get_canonical_isbn('9780826497520'), None)
