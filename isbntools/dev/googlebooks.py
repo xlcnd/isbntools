d193eb69e12b7a7417ab88db02510c175ffb6edf
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-13 14:44:35 +0000

    code comments

--- isbntools/dev/googlebooks.py
+++ isbntools/dev/googlebooks.py
@@ -24,12 +24,14 @@ class GOOBQuery(WEBQuery):
         """
         self.isbn = isbn
         WEBQuery.__init__(self, SERVICE_URL % isbn, UA)
+        # lets us go with the default raw data_checker
         WEBQuery.check_data(self)
 
     def records(self):
         """
         Classifies (canonically) the parsed data
         """
+        # this service uses JSON, so stay with the default parser
         data = WEBQuery.parse_data(self)
         try:
             # put the selected data in records
--- isbntools/dev/wcated.py
+++ isbntools/dev/wcated.py
@@ -25,12 +25,14 @@ class WCATEdQuery(WEBQuery):
         """
         self.isbn = isbn
         WEBQuery.__init__(self, SERVICE_URL % isbn, UA)
+        # lets us go with the default raw data_checker
         WEBQuery.check_data(self)
 
     def records(self):
         """
         Returns the records from the parsed response
         """
+        # this service sends Python, so pass the ast.literal_eval parser
         data = WEBQuery.parse_data(self, parser=literal_eval)
         try:
             # put the selected data in records
