de92e7c9bd7ce9b11b09b1a83f848dd6cd66f36d
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-13 16:02:31 +0000

    flake8 ok

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -6,5 +6,5 @@ __version__ = '2.0.6'
 
 from .exceptions import quiet_errors
 from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
-    canonical, notisbn, get_isbnlike, get_canonical_isbn)
+                   canonical, notisbn, get_isbnlike, get_canonical_isbn)
 from .ext import (mask, meta, info, editions)
