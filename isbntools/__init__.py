149f039d04cc3c16e0f63a07d47f145b2d13b2a6
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-14 13:05:40 +0000

    added new method isbn_from_words

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
-           'canonical', 'get_canonical_isbn', 'editions', 'quiet_errors']
+           'canonical', 'get_canonical_isbn', 'editions', 'goos', 'quiet_errors']
 
 __version__ = '2.0.6'
 
 from .exceptions import quiet_errors
 from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
                    canonical, notisbn, get_isbnlike, get_canonical_isbn)
-from .ext import (mask, meta, info, editions)
+from .ext import (mask, meta, info, editions, isbn_from_words)
