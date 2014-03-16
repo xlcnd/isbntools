4be84859270b8b6bc2023bdbf3ce1b92b2d9fd86
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-16 17:42:46 +0000

    socket and threads timeouts in config

--- isbntools/config.py
+++ isbntools/config.py
@@ -1,6 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """
+Config file for isbntools
+"""
+
+
+"""
+Timeouts
+"""
+
+SOCKETS_TIMEOUT = 12    # seconds
+THREADS_TIMEOUT = 11    # seconds
+
+
+"""
 Your API keys
 """
 apikeys = {}
@@ -10,6 +23,6 @@ def add_apikey(service, apikey):
     """
     Add API keys
 
-    ex: add_apikey('isbndb', 'JuHytr6') [is fake!]
+    add_apikey('isbndb', 'JuHytr6') [is fake!]
     """
     apikeys[service] = apikey
--- isbntools/metadata.py
+++ isbntools/metadata.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 
 import socket
+from . import config
 from .registry import services
 from .exceptions import NotRecognizedServiceError
 
 # socket timeout is not exposed at urllib2 level so I had to import the
 # module and set a default value for all the sockets (timeout in seconds)
 # however this should be done at top level due to strong side effects...
-socket.setdefaulttimeout(10)
+socket.setdefaulttimeout(config.SOCKETS_TIMEOUT)
 
 
 def query(isbn, service='default'):
