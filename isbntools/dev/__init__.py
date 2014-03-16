74b7d073c036dc679efb0e89035e2be9f3a153b7
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-16 22:57:09 +0000

    a new provide `openl` (openlibrary.org)

--- isbntools/dev/__init__.py
+++ isbntools/dev/__init__.py
@@ -1,10 +1,10 @@
 __all__ = ['webservice', 'webquery', 'exceptions',
-           'wcat', 'wcated', 'googlebooks', 'isbndb',
+           'wcat', 'wcated', 'googlebooks', 'isbndb', 'openl',
            'WSHTTPError', 'WSURLError', 'WQDataNotFoundError',
            'WQServiceIsDownError', 'WPDataWrongShapeError',
            'WPNotValidMetadataError', 'Metadata', 'stdmeta',
            'normalize_space', 'WEBService', 'WEBQuery', 'WCATQuery',
-           'WCATEdQuery', 'GOOBQuery', 'ISBNDBQuery',
+           'WCATEdQuery', 'GOOBQuery', 'ISBNDBQuery', 'OPENLQuery',
            'WSHTTPError', 'WSURLError',
            'WQDataNotFoundError', 'WQServiceIsDownError',
            'WPDataWrongShapeError', 'WPNotValidMetadataError',
@@ -18,6 +18,7 @@ from .wcat import WCATQuery
 from .wcated import WCATEdQuery
 from .googlebooks import GOOBQuery
 from .isbndb import ISBNDBQuery
+from .openl import OPENLQuery
 from .exceptions import (WSHTTPError, WSURLError,
                          WQDataNotFoundError, WQServiceIsDownError,
                          WPDataWrongShapeError, WPNotValidMetadataError,
