acb18b0bcd669c7a5edea50d2684602fbb16236b
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-17 22:22:44 +0000

    create `setconf`

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,7 +1,7 @@
 __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
            'canonical', 'get_canonical_isbn', 'editions', 'isbn_from_words',
-           'quiet_errors', 'config']
+           'quiet_errors', 'config', 'setconf']
 
 __version__ = '2.1.2'
 
@@ -10,3 +10,4 @@ from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
                    canonical, notisbn, get_isbnlike, get_canonical_isbn)
 from .ext import (mask, meta, info, editions, isbn_from_words)
 from . import config
+from . import setconf
