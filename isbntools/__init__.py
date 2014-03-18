bc78f8be0894a89eb7c25070ff23a18f03a1a40f
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-18 12:09:39 +0000

    init v2.2.1

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -3,7 +3,7 @@ __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'canonical', 'get_canonical_isbn', 'editions', 'isbn_from_words',
            'quiet_errors', 'config', 'setconf']
 
-__version__ = '2.1.2'
+__version__ = '2.2.1'
 
 from .exceptions import quiet_errors
 from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
