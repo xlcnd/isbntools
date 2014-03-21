c6c2916cfdb616bcfe21159423b609a17bd955a7
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-21 08:07:52 +0000

    ren canonical to value

--- isbntools/test/test_data.py
+++ isbntools/test/test_data.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-""" nose tests 
- 
+""" nose tests
+
 """
 
 from ..dev import Metadata, stdmeta
@@ -12,62 +12,62 @@ from nose.tools import assert_equals, assert_raises
 def test_stdmeta():
     # test stdmeta from data
     r={
-       'ISBN-13': u'9780123456789 ', 
-       'Title': u'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': u'2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789 ',
+       'Title': u'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': u'2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2   ']
        }
     R={
-       'ISBN-13': u'9780123456789', 
-       'Title': u'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': u'2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789',
+       'Title': u'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': u'2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2']
        }
     A={
-       'ISBN-13': u'9780123456789 ', 
-       'Title': 'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': '2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789 ',
+       'Title': 'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': '2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2   ']
-       }   
+       }
     assert_equals(stdmeta(r), R)
     assert_equals(stdmeta(R), R)
     assert_raises(Exception, stdmeta, A)
 
 def test_metaclass():
     R={
-       'ISBN-13': u'9780123456789', 
-       'Title': u'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': u'2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789',
+       'Title': u'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': u'2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2']
        }
     A={
-       'ISBN-13': u'9780123456789', 
-       'Title': u'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': u'2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789',
+       'Title': u'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': u'2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2']
        }
     B={
-       'ISBN-13': u'9780123456789', 
-       'Title': u'Bla. Bla /Title .', 
-       'Publisher': u'', 
-       'Year': u'2000', 
-       'Language': u'en', 
+       'ISBN-13': u'9780123456789',
+       'Title': u'Bla. Bla /Title .',
+       'Publisher': u'',
+       'Year': u'2000',
+       'Language': u'en',
        'Authors': [u'author1. mba', u'author2', u'Bambini']
        }
-    dt=Metadata(R);  
-    assert_equals(dt.canonical, R)
+    dt=Metadata(R);
+    assert_equals(dt.value, R)
     dt.add_to_authors(u'Bambini')
-    assert_equals(dt.canonical, B)
+    assert_equals(dt.value, B)
 
 
 # flake8: noqa
