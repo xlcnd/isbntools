e135595ef60cc85155dde14f8751b1fe14d54f92
Alexandre Conde <alexandreconde@hotmail.com>
2014-03-23 18:08:37 +0000

    check_updates online

--- isbntools/version.py
+++ isbntools/version.py
@@ -6,7 +6,7 @@ def check_version():
     try:
         from isbntools import __version__
 
-        UA = "Mozilla/5.0 (X11; Ubuntu; Linux i686; rv:16.0) Firefox/17.0"
+        UA = "isbntools %s" % __version__
         headers = {'User-Agent': UA}
 
         url = "https://raw.githubusercontent.com/xlcnd/"\
@@ -20,7 +20,7 @@ def check_version():
 
         if __version__ != newversion:
             print((" **A new version (%s) is available!**" % newversion))
-            print(r" Enter at command line: [sudo] pip install -U isbntools")
+            print(r" At command line enter: [sudo] pip install -U isbntools")
             print(r"    or")
             print(r" Download it from https://pypi.python.org/pypi/isbntools")
     finally:
