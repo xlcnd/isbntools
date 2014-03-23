65025ebd2fb3f80774210aca3816050a9743dfd1
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-23 15:51:39 +0000

    check_updates online

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,7 +1,8 @@
 __all__ = ['is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'to_isbn10', 'to_isbn13', 'get_isbnlike', 'notisbn',
            'canonical', 'get_canonical_isbn', 'editions', 'isbn_from_words',
-           'quiet_errors', 'config', 'setconf']
+           'quiet_errors', 'config', 'setconf', '__version__',
+           'check_version']
 
 __version__ = '2.3.2'
 
@@ -11,3 +12,4 @@ from .core import (is_isbn10, is_isbn13, to_isbn10, to_isbn13, clean,
 from .ext import (mask, meta, info, editions, isbn_from_words)
 from . import config
 from . import setconf
+from .version import check_version
