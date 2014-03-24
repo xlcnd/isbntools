8c8edbf2c213df452d1ae28e9881237d154497ee
Alexandre Conde <alexandrelimaconde@gmail.com>
2014-03-24 21:42:51 +0000

    setuptools

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
-from distutils.core import setup
+from setuptools import setup
 from isbntools import __version__
 
 setup(
