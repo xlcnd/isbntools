43d8de6a718a56e35903f91615bcb6ae850fe562
Alexandre Conde <alexandreconde@hotmail.com>
2014-04-08 17:26:46 +0100

    more keywords

--- setup.py
+++ setup.py
@@ -58,7 +58,8 @@ setup(
     license='LGPL v3',
     description='Extract, clean, transform, hyphenate and metadata for ISBNs (International Standard Book Number).',
     long_description=open('README.rst').read(),
-    keywords='ISBN, validate, transform, hyphenate, metadata, World Catalogue, Google Books, Open Library, isbndb.com',
+    keywords='ISBN, validate, transform, hyphenate, metadata, World Catalogue,
+    Google Books, Open Library, isbndb.com, BibTeX, EndNote, RefWorks',
     classifiers=[
         'Programming Language :: Python :: 2.7',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
