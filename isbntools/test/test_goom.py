5111b738427b922b4433c308edfbb1a3c34a672f
Alexandre Conde <alexandreconde@hotmail.com>
2014-04-26 11:21:03 +0100

    clean

--- isbntools/test/test_goom.py
+++ isbntools/test/test_goom.py
@@ -1,15 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # flake8: noqa
-""" 
+"""
 nose tests
 """
 
 from ..dev import goom
-from nose.tools import assert_equals, assert_raises
+from nose.tools import assert_equals
 
 
 def test_goom():
     assert_equals(len(repr(goom.query('the old man and the sea'))) > 1000, True)
-
-
