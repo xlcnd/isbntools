0b6817696fd9985593f9005dbb00213f4265bb8b
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-05 15:12:23 +0100

    add unicode_to_utf8tex to helpers (closes #29)

--- isbntools/dev/helpers.py
+++ isbntools/dev/helpers.py
@@ -25,3 +25,17 @@ def titlecase(s):
     """
     return re.sub(r"[A-Za-z]+('[A-Za-z]+)?",
                   lambda m: m.group(0)[0].upper() + m.group(0)[1:], s)
+
+
+def unicode_to_utf8tex(ustream, filtre=None):       # pragma: no cover
+    """
+    Replaces unicode entities by tex entitites and returns utf8 bytes
+    """
+    from ..bouth23 import b, s
+    from ..data.data4tex import unicode_to_tex
+    filtre = filtre if filtre else []
+    bstream = ustream.encode('utf-8')
+    table = dict((k.encode('utf-8'), v) for k, v in unicode_to_tex.items()
+                 if v not in filtre)
+    regex = re.compile(b('|'.join(re.escape(s(k)) for k in table)))
+    return regex.sub(lambda m: b(table[m.group(0)]), bstream)
