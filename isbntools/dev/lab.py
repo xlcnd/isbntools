c30a9a1c3f9a25e250a3d280a34a59bd1ae58aab
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-06 21:45:49 +0100

    add fmts

--- isbntools/dev/lab.py
+++ isbntools/dev/lab.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 # flake8:noqa
 # pylint: skip-file
-from .fmt import fmtbib
+from .fmt import fmtbib, fmts
 from .helpers import unicode_to_utf8tex as to_utf8tex
 from .helpers import normalize_space
