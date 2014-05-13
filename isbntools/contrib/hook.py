32d4555ca17113ef521c9a3a30890f81ff1773c7
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-13 22:04:18 +0100

    add to contrib.hook helpers for user script

--- isbntools/contrib/hook.py
+++ isbntools/contrib/hook.py
@@ -1,6 +1,8 @@
 # flake8: noqa
 # pylint: skip file
 import os
+import sys
+
 try:
     from ..__init__ import __version__, pkg_path, defaults_conf
     from ..setconf import conf
@@ -9,8 +11,8 @@ try:
     pkg_options = conf.items('MODULES') if conf.has_section('MODULES') else []
     conf_file = conf.files[-1] if conf.files else os.path.join(pkg_path, defaults_conf)
 
-    __all__ = ['pkg_version', 'pkg_path', 'pkg_options', 'write_conf', 'reg_plugin']
-
+    __all__ = ['pkg_version', 'pkg_path', 'pkg_options', 'write_conf',
+               'reg_plugin', 'reg_apikey', 'mk_conf', 'print_conf']
 except:
     pass
 
@@ -28,11 +30,51 @@ def write_conf(opts):
     __write2conf('MODULES', opts)
 
 
+def reg_apikey(service, api_key):
+    __write2conf('SERVICES', {service.upper()+'_API_KEY': api_key})
+
+
 def reg_plugin(name, api_key=None, path=None):
     path = path if path else name + '.py'
     __write2conf('PLUGINS', {name: path})
     if api_key:
-        __write2conf('SERVICES', {name.upper()+'_API_KEY': api_key})
+        reg_apikey(name, pi_key)
 
 
+def __in_virtual():
+    return True if hasattr(sys, 'real_prefix') else False
 
+
+def __mkpath(path):
+    directory = os.path.dirname(path)
+    filename = os.path.basename(path)
+    if directory and not os.path.exists(directory):
+        os.makedirs(directory)
+    if filename:
+        with open(path, 'w+') as f:
+            f.close()
+
+
+def __conf_file():
+    if __in_virtual():
+        installpath = ''
+    else:
+        homepath = os.path.expanduser('~') if os.name != 'nt' else os.getenv('APPDATA')
+        confdir = '.isbntools' if os.name != 'nt' else 'isbntools'
+        installpath = os.path.join(homepath, confdir)
+    conffile = 'isbntools.conf'
+    return os.path.join(installpath, conffile)
+
+
+def mk_conf():
+    if conf_file.endswith('.py') or not os.path.exists(__conf_file()):
+        __mkpath(__conf_file())
+        with open(__conf_file(), 'wb') as f:
+            conf.write(f)
+
+
+def print_conf():
+    if conf_file.endswith('.py'):
+        return
+    print(("conf file at %s:" % conf_file))
+    conf.write(sys.stdout)
