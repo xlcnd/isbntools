5a8d455c17ba08e5fb6180577ff06aa6f4b3a596
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-19 21:41:55 +0100

    add test for `cutoff_tokens`

--- isbntools/test/test_helpers.py
+++ isbntools/test/test_helpers.py
@@ -5,7 +5,7 @@
 nose tests
 """
 
-from ..dev.helpers import last_first
+from ..dev.helpers import last_first, cutoff_tokens
 from nose.tools import assert_equals
 
 
@@ -13,3 +13,7 @@ def test_last_first():
     assert_equals(last_first("Surname, First Name"), {"last": "Surname", "first": "First Name"})
     assert_equals(last_first("First Name Surname"), {"last": "Surname", "first": "First Name"})
     assert_equals(last_first("Surname1, First1 and Sur2, First2"), {"last": "Surname1", "first": "First1 and Sur2, First2"})
+
+
+def test_cutoff_tokens():
+    assert_equals(cutoff_tokens(['1', '23', '456'], 3), ['1', '23'])
