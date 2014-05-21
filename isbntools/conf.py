8a2a189f631204f775c613e0df55e31f4e3f4042
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-21 14:16:24 +0100

    pep8

--- isbntools/conf.py
+++ isbntools/conf.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 from .contrib.hook import (pkg_version, pkg_path, pkg_options, reg_mod,
-                           reg_plugin, reg_apikey, mk_conf, print_conf, reg_myopt)
+                           reg_plugin, reg_apikey, mk_conf, print_conf,
+                           reg_myopt)
 
 __all__ = ['pkg_version', 'pkg_path', 'pkg_options', 'reg_mod',
            'reg_plugin', 'reg_apikey', 'mk_conf', 'print_conf', 'reg_myopt']
-
-
-
