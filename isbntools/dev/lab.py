f2e8eebc0ec12f733d990dcaa0ba84a050aa3e1c
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-21 18:03:45 +0100

    keep `bouth23` in core

--- isbntools/dev/lab.py
+++ isbntools/dev/lab.py
@@ -5,4 +5,3 @@ from ._fmt import fmtbib, fmts
 from ._helpers import unicode_to_utf8tex as to_utf8tex
 from ._helpers import normalize_space, last_first, in_virtual, cutoff_tokens
 from ._files import File, cwdfiles
-from .. import bouth23
