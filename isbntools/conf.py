985a6c3d9c7997816336ef62f8f5f0e6d45794ee
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-23 19:15:41 +0100

    req pep257

--- isbntools/conf.py
+++ isbntools/conf.py
@@ -3,5 +3,5 @@ from .contrib.hook import (pkg_version, pkg_path, pkg_options, reg_mod,
                            reg_plugin, reg_apikey, mk_conf, print_conf,
                            reg_myopt)
 
-__all__ = ['pkg_version', 'pkg_path', 'pkg_options', 'reg_mod',
-           'reg_plugin', 'reg_apikey', 'mk_conf', 'print_conf', 'reg_myopt']
+__all__ = ('pkg_version', 'pkg_path', 'pkg_options', 'reg_mod',
+           'reg_plugin', 'reg_apikey', 'mk_conf', 'print_conf', 'reg_myopt')
