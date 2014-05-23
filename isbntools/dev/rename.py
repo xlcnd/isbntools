102b9954c6be476d86956f3e23cd6a49f20a30fc
Christoph Mack <christophmack@web.de>
2014-05-23 11:19:07 +0200

    added dev and bin tool for renaming

---
+++ isbntools/dev/rename.py
@@ -0,0 +1,72 @@
+# -*- coding: utf-8 -*-
+
+import string
+import logging
+
+from ._helpers import last_first
+from isbntools.bouth23 import u
+
+LOGGER = logging.getLogger(__name__)
+
+
+def checkpattern(pattern):
+    """
+    checks a pattern for renaming a file for validity
+    """
+
+    placeholders = ['{authorsFullNames}', '{authorsLastNames}',
+                    '{firstAuthorLastName}', '{year}', '{publisher}',
+                    '{title}', '{isbn}', '{language}']
+    tocheck = pattern[:]
+
+    placeholderfound = False
+    for placeholder in placeholders:
+        if placeholder in tocheck:
+            tocheck = tocheck.replace(placeholder, '')
+            placeholderfound = True
+    if not placeholderfound:
+        LOGGER.warning('no placeholders found in pattern \'' + pattern + '\'')
+        return False
+
+    validchars = '-_.,() {0}{1}'.format(string.ascii_letters, string.digits)
+    for char in tocheck:
+        if char not in validchars:
+            LOGGER.warning('invalid character in pattern: \'' + char + '\'')
+            return False
+    return True
+
+
+def newfilename(metadata, pattern='{authorsLastNames}_{year}_{title}_{isbn}'):
+    """
+    returns a new file name created from
+    book metadata formatted according to pattern
+    """
+
+    for key in metadata.keys():
+        if not metadata[key]:
+            metadata[key] = u('UNKNOWN')
+
+    d = {
+        'authorsFullNames': ','.join(metadata['Authors']),
+        'year': metadata['Year'],
+        'publisher': metadata['Publisher'],
+        'title': metadata['Title'],
+        'language': metadata['Language'],
+        'isbn': metadata['ISBN-13']
+    }
+
+    authorslastnames = [last_first(authorname)['last']
+                        for authorname in metadata['Authors']]
+    d['authorsLastNames'] = ','.join(authorslastnames)
+    d['firstAuthorLastName'] = authorslastnames[0]
+
+    try:
+        formatted = u(pattern).format(**d)
+        return cleannewname(formatted)
+    except KeyError as e:
+        LOGGER.warning('unknown placeholder: ' + str(e))
+
+
+def cleannewname(newname):
+    newname = newname.strip('.,_')
+    return newname.strip()
---
+++ isbntools/test/test_rename.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from nose.tools import assert_equals
+from isbntools.dev.rename import checkpattern, newfilename
+
+"""
+nose tests
+"""
+
+
+def test_rename_dev():
+    assert_equals(checkpattern(
+                  '{authorsLastNames}_{year}_{title}_{isbn}.pdf'), True)
+    assert_equals(checkpattern('mypattern.pdf'), False)
+    assert_equals(checkpattern('mypattern{year}'), True)
+    assert_equals(checkpattern(
+                  '.{authorsFullNames}_{authorsLastNames} {year}..'
+                  '{publisher}({title}){isbn}{language}'), True)
+    assert_equals(checkpattern('authors:{authorsFullNames}'), False)
+
+    metadata = {'Title': 'A Dictionary Of The Internet',
+                'Authors': ['Darrel Ince', 'Oxford University Press'],
+                'Publisher': 'Oxford University Press',
+                'ISBN-13': '9780199571444', 'Language': 'eng', 'Year': '2009'}
+    assert_equals(newfilename(
+                  metadata,
+                  '{authorsLastNames}_{year}_{title}_{isbn}.epub'),
+                  'Ince,Press_2009_A Dictionary Of '
+                  'The Internet_9780199571444.epub')
+    assert_equals(
+        newfilename(metadata, '{authorsFullNames}_{publisher}_{language}'),
+        'Darrel Ince,Oxford University Press_Oxford University Press_eng')
+    assert_equals(newfilename(metadata,
+                  'myfile_{year} {authorsLastNames}.pdf'),
+                  'myfile_2009 Ince,Press.pdf')
+    assert_equals(newfilename(metadata, 'myfile_{nokey}'), None)
+    assert_equals(newfilename(metadata, '{authorsFullNames}: {title}'),
+                  'Darrel Ince,Oxford University Press: A '
+                  'Dictionary Of The Internet')
+    assert_equals(newfilename(metadata, 'myfile.pdf'), 'myfile.pdf')
+
+    metadata['Publisher'] = ''
+    assert_equals(newfilename(metadata,
+                  pattern='{authorsFullNames}_{publisher}_{language}'),
+                  'Darrel Ince,Oxford University Press_UNKNOWN_eng')
