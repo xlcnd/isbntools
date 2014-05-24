e3b480b41bc0ac3f06305781e63d7a3f0f7bf97a
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-24 12:25:35 +0100

    add doc string pep257 (#51)

--- isbntools/_ext.py
+++ isbntools/_ext.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+"""Extra methods."""
 
 from ._metadata import query
 from ._infogroup import infogroup
@@ -10,34 +10,31 @@ from ._core import EAN13
 
 
 def mask(isbn, separator='-'):
-    """ `Masks` a canonical ISBN """
+    """`Mask` a canonical ISBN."""
     return msk(isbn, separator)
 
 
 def meta(isbn, service='default'):
-    """ Metadata from worldcat.org ('wcat'), Google Books ('goob') , ..."""
+    """Get metadata from worldcat.org ('wcat'), Google Books ('goob') , ..."""
     return query(isbn, service)
 
 
 def info(isbn):
-    """ Language or country assigned to this ISBN """
+    """Get language or country assigned to this ISBN."""
     return infogroup(isbn)
 
 
 def editions(isbn):
-    """ Returns the list of ISBNs of editions related with this ISBN """
+    """Return the list of ISBNs of editions related with this ISBN."""
     return qed(isbn)
 
 
 def isbn_from_words(words):
-    """ Returns the most probable ISBN from a list of words from title
-        and author's name (uses Google Search)
-    """
+    """Return the most probable ISBN from a list of words."""
     return goos(words)
 
 
 def doi(isbn):
-    """ Returns a DOI's ISBN-A from a ISBN-13
-    """
+    """Return a DOI's ISBN-A from a ISBN-13."""
     return "10.%s.%s%s/%s%s" % \
            tuple(msk(EAN13(isbn), '-').split('-'))
