ac8452a96fedf38d675f87ff26dabeff6a72f024
Alexandre Conde <alexandreconde@hotmail.com>
2014-05-30 17:43:06 +0100

    add more tests to 'test_fmt'

--- isbntools/test/test_fmt.py
+++ isbntools/test/test_fmt.py
@@ -21,3 +21,9 @@ canonical = {
 
 def test_fmtbib():
     assert_equals(len(fmtbib("bibtex", canonical)), 182)
+    assert_equals(len(fmtbib("labels", canonical)), 158)
+    assert_equals(len(fmtbib("endnote", canonical)), 103)
+    assert_equals(len(fmtbib("msword", canonical)), 485)
+    assert_equals(len(fmtbib("json", canonical)), 213)
+    assert_equals(len(fmtbib("refworks", canonical)), 130)
+    
