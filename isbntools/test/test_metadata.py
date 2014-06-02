77ece455fc99e92d2efe7eb3df4a8780d14baf9f
Alexandre Conde <alexandreconde@hotmail.com>
2014-06-02 19:23:57 +0100

    test with no cache

--- isbntools/test/test_metadata.py
+++ isbntools/test/test_metadata.py
@@ -7,7 +7,6 @@
 D25 |-> RELEASE MINIMAL TESTING. (I DON'T WANTED TO BE BLACKLISTED BY THE SERVICES) <-|
 """
 
-from random import randrange
 from .._metadata import query
 from .._ext import meta
 from nose.tools import assert_equals, assert_raises
@@ -24,7 +23,7 @@ def test_query():
     assert_equals(len(repr(query('9780321534965', 'goob'))) in (154, 160, 189, 195), True)
     assert_equals(len(repr(query('9789934015960'))) in (166, 187), True)
     assert_equals(len(repr(query('9781118241257'))) in (177, 183), True)
-    assert_raises(Exception, query, randrange(0, 10000000), 'wcat', None)
+    assert_raises(Exception, query, '9780000000', 'wcat', None)
 
 
 def test_ext_meta():
@@ -33,4 +32,4 @@ def test_ext_meta():
     assert_equals(len(repr(meta('9780321534965', 'wcat'))) in (252, 258), True)
     assert_equals(len(repr(meta('9780321534965', 'merge'))) in (173, 179), True)
     assert_equals(len(repr(meta('9780321534965'))) in (173, 179), True)
-    assert_raises(Exception, meta, randrange(0, 10000000), 'wcat', None)
+    assert_raises(Exception, meta, '9780000000', 'wcat', None)
