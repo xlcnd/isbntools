68640b6da9e55cc6a67bee6b24b78a68d7734ca5
Alexandre Conde <alexandreconde@hotmail.com>
2014-06-16 11:42:15 +0100

    init v3.3.4

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -6,7 +6,7 @@ __all__ = ('is_isbn10', 'is_isbn13', 'clean', 'mask', 'info', 'meta',
            'quiet_errors', 'config', 'setconf', '__version__', '__support__',
            'doi', 'check_version', 'ISBN13', 'Cache', 'in_virtual')
 
-__version__ = '3.3.3'                               # <-- literal IDs
+__version__ = '3.3.4'                               # <-- literal IDs
 __support__ = 'py26, py27, py33, py34, pypy'        # <-- literal IDs
 
 
