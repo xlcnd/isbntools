ea4315708854013bad8c436041e66a798f506ae2
Alexandre Conde <alexandreconde@hotmail.com>
2014-06-25 09:43:06 +0100

    fix whitespace

--- isbntools/dev/lab.py
+++ isbntools/dev/lab.py
@@ -9,5 +9,4 @@ from ._helpers import (normalize_space, last_first,
                        cutoff_tokens, parse_placeholders)
 from ._files import File, cwdfiles
 from ._rename import renfile, rencwdfiles
-
 from ._imcache import IMCache
