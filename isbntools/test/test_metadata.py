07a2f5a5b7299f0571ad7181d4fd1e802733106e
Alexandre Conde <alexandreconde@hotmail.com>
2014-08-06 13:50:04 +0100

    more robust test condition

--- isbntools/test/test_metadata.py
+++ isbntools/test/test_metadata.py
@@ -1,11 +1,8 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # flake8: noqa
 # pylint: skip-file
 
-""" nose tests
-D25 |-> RELEASE MINIMAL TESTING. (I DON'T WANTED TO BE BLACKLISTED BY THE SERVICES) <-|
-"""
+""" nose tests """
 
 from random import randrange
 from .._metadata import query
@@ -16,13 +13,13 @@ from nose.tools import assert_equals, assert_raises
 def test_query():
     # test query from metadata
     assert_raises(Exception, query, '9780321534965', 'goog')
-    assert_equals(len(repr(query('9781849692342', 'goob'))) in (201, 208), True)
+    assert_equals(len(repr(query('9781849692342', 'goob'))) > 200, True)
     assert_raises(Exception, query, '9781849692341', 'wcat')
     assert_equals(len(repr(query('9780321534965', 'wcat'))) > 150, True)
     assert_equals(len(repr(query('9780321534965'))) > 150, True)
-    assert_equals(len(repr(query('9780321534965', 'merge'))) in (173, 179), True)
-    assert_equals(len(repr(query('9780321534965', 'goob'))) in (154, 160, 189, 195), True)
-    assert_equals(len(repr(query('9789934015960'))) in (166, 187), True)
+    assert_equals(len(repr(query('9780321534965', 'merge'))) > 150), True)
+    assert_equals(len(repr(query('9780321534965', 'goob'))) > 150, True)
+    assert_equals(len(repr(query('9789934015960'))) > 150, True)
     assert_equals(len(repr(query('9781118241257'))) > 150, True)
     assert_raises(Exception, query, '9780000000', 'wcat', None)
     assert_raises(Exception, query, randrange(0, 1000000), 'wcat')
@@ -30,9 +27,9 @@ def test_query():
 
 def test_ext_meta():
     # test meta from core
-    assert_equals(len(repr(meta('9781849692342', 'goob'))) in (201, 208), True)
+    assert_equals(len(repr(meta('9781849692342', 'goob'))) > 200, True)
     assert_equals(len(repr(meta('9780321534965', 'wcat'))) > 150, True)
-    assert_equals(len(repr(meta('9780321534965', 'merge'))) in (173, 179), True)
+    assert_equals(len(repr(meta('9780321534965', 'merge'))) > 150, True)
     assert_equals(len(repr(meta('9780321534965'))) > 150, True)
     assert_raises(Exception, meta, '9780000000', 'wcat', None)
     assert_raises(Exception, meta, randrange(0, 1000000), 'wcat')
