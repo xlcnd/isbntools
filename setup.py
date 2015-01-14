d1104e9022d77fb3bbdf789a2a9b7f8593b5dabd
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-14 14:12:30 +0000

    require isbnlib-3.4.3

--- setup.py
+++ setup.py
@@ -110,7 +110,7 @@ setup(
                             'isbntools=isbntools.bin.version:main']
     },
     data_files=data_files,
-    install_requires=["isbnlib>=3.4.2"],
+    install_requires=["isbnlib>=3.4.3"],
     license='LGPL v3',
     description='Extract, clean, transform, hyphenate and metadata for ISBNs (International Standard Book Number).',
     long_description=open('README.rst').read(),
