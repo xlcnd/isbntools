40814095811841477a8c96506ae6717f426794f4
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-16 14:25:29 +0000

    delete isbnlib tests

--- isbntools/test/test_words.py
+++ isbntools/test/test_words.py
@@ -5,10 +5,10 @@
 nose tests
 """
 
-from .. import _gwords as words
+from .. import isbn_from_words
 from nose.tools import assert_equals
 
 
 def test_words():
-    assert_equals(len(words.goos('the old man and the sea')), 13)
-    assert_equals(words.goos('-ISBN -isbn') in ('9781364200329', None), True)
+    assert_equals(len(isbn_from_words('the old man and the sea')), 13)
+    assert_equals(isbn_from_words('-ISBN -isbn') in ('9781364200329', None), True)
