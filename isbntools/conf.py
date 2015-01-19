cf34a7bfd8af25ecdf4248d0c8d7d3f90d3f7308
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-19 19:10:20 +0000

    complete docstring

--- isbntools/conf.py
+++ isbntools/conf.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""Edit conf file."""
+"""Edit conf file (mainly for programatic API)."""
 
 from ._hook import (pkg_version, pkg_path, pkg_options, reg_mod,
                     reg_plugin, reg_apikey, mk_conf, print_conf,
