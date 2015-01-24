52c002aad91eac04c24fe99da9a0f2c1a895fbd9
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-24 14:41:18 +0000

    unicode encoding in Windows

--- isbntools/_lab.py
+++ isbntools/_lab.py
@@ -11,19 +11,14 @@ from isbnlib.dev.bouth23 import b2u3
 WINDOWS = os.name == 'nt'
 
 
-def b2s3(x):
-    """For Windows."""
-    return x.encode("utf-8") if sys.version < '3' else x.decode("utf-8", 'ignore')
-
-
 def sprint(content):
     """Smart print function so that redirection works... (see issue 75)."""
     if WINDOWS:  # pragma: no cover
         # print detects the appropriate code
         # (Windows terminal doesn't use UTF-8)
-        # print(content.encode("utf-8"))
-        s = content + '\n'
-        sys.stdout.write(b2s3(s))
+        print(content)
+        # s = content + '\n'
+        # sys.stdout.write(b2s3(s))
     else:
         # stdout gets UTF-8
         s = content + '\n'
