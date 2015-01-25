aa504211cda924ca2bb10312f8eeb5f7501e3134
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-25 11:09:12 +0000

    unify sprint

--- isbntools/_lab.py
+++ isbntools/_lab.py
@@ -6,19 +6,16 @@
 import os
 import sys
 
-from isbnlib.dev.bouth23 import u
+# from isbnlib.dev.bouth23 import u
 
 WINDOWS = os.name == 'nt'
-EOL = u('\r\n') if WINDOWS else u('\n')
 PY2 = sys.version < '3'
+EOL = '\r\n' if WINDOWS and not PY2 else '\n'
 
 
 def sprint(content):
     """Smart print function so that redirection works... (see issue 75)."""
-    # FIXME adopt the same solution for both ops
     s = content + EOL
-    # the print function doesn't work well with redirection
-    # is best to work with bytes (unicode encoded as UTF-8)
     buf = s.encode("utf-8")
     if PY2:
         sys.stdout.write(buf)
