e175a3ded015c55384fe5757020372306c7470f9
Alexandre Conde <alexandreconde@hotmail.com>
2015-01-29 17:03:03 +0000

    add a 'bad version' condition

--- setup.py
+++ setup.py
@@ -30,6 +30,23 @@ if tuple(int(x) for x in sys.version[:3].split('.')) not in SUPPORTED:
     raise Exception('isbntools %s  requires Python 2.6+ or 3.3+.' %
                     __version__)
 
+# don't install if the installed version is 4.0.1
+try:
+    from isbntools.app import version  # <-- signature for 4.0.1
+    BAD_VERSION = True
+except ImportError:
+    BAD_VERSION = False
+
+if BAD_VERSION:
+    print("Installation aborted! (Version '4.0.1' found).")
+    print("I am sorry but, in order to upgrade 'isbntools', you need to:")
+    print("1. enter 'sudo pip uninstall -y isbntools' at your terminal")
+    print("2. make sure that all files and folders with name 'isbntools' in it")
+    print("   have been deleted from your 'site-packages' folder")
+    print("THEN you can do a fresh install... thanks.")
+    sys.exit()
+
+
 # ENV
 
 ARGVS = sys.argv
