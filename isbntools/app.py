ca86501b75a690e8b7127cdf2c7514a3a1e91fcd
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-03 22:23:59 +0000

    del some unnecessary imports from isbnlib

--- isbntools/app.py
+++ isbntools/app.py
@@ -1,10 +1,10 @@
 """Define isbntools API and set lib environment."""
 
 __all__ = ('check_version', 'audit', 'messages',
-           'ISBNToolsException', 'canonical', 'clean', 'EAN13',
-           'doi', 'editions', 'get_canonical_isbn', 'get_isbnlike',
-           'info', 'is_isbn10', 'is_isbn13', 'isbn_from_words',
-           'mask', 'meta', 'notisbn', 'to_isbn10', 'to_isbn13', 'goom',
+           'ISBNToolsException', 'EAN13',
+           'doi', 'editions', 'get_canonical_isbn',
+           'info', 'isbn_from_words', 'get_isbnlike',
+           'mask', 'meta', 'to_isbn10', 'to_isbn13', 'goom',
            'libversion', 'config', 'registry', 'doi2tex', 'quiet_errors',
            'CONF_PATH', 'CACHE_FILE')
 
@@ -14,20 +14,15 @@ import os as _os
 pkg_path = _os.path.dirname(_os.path.abspath(__file__))
 defaults_conf = 'DEFAULTS'
 
-# isbnlib hook
-from isbnlib import (canonical, clean, doi, editions, get_canonical_isbn,
-                     get_isbnlike, info, is_isbn10, is_isbn13,
-                     isbn_from_words, mask, meta, notisbn,
+# isbnlib hook -- only top level functions
+from isbnlib import (canonical, clean, doi, editions,
+                     info, get_isbnlike, get_canonical_isbn,
+                     isbn_from_words, mask, meta,
                      to_isbn10, to_isbn13, EAN13, goom, doi2tex, quiet_errors)
 from isbnlib import __version__ as libversion
 
-# inject isbntools dependencies on isbnlib
-from . import _initapp         # <-- first import after lib (IMPORTANT)
-# from isbnlib import config
-# from isbnlib import registry
-from ._initapp import config
-from ._initapp import registry
-from ._initapp import CONF_PATH, CACHE_FILE
+# inject isbntools dependencies on isbnlib <-- first import after lib (IMPORTANT)
+from ._initapp import config, registry, CONF_PATH, CACHE_FILE
 
 # isbntools
 from .contrib.modules.report import check_version, audit, messages
