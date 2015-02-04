922396018547d3f3c852c892a9e13df7b7d1296a
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-04 15:16:50 +0000

    doitotex -> doi2tex

---
+++ isbntools/bin/doi2tex.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# noqa
+
+import sys
+
+from isbntools.app import config, doi2tex, quiet_errors
+
+
+TO = 30
+PREFIX = 'isbn_'
+
+
+def usage(prefix=PREFIX):
+    print('Usage: %sdoi2tex DOI' % prefix)
+    return 1
+
+
+def main(doi=None, prefix=PREFIX):
+    sys.excepthook = quiet_errors
+    config.setsocketstimeout(TO)
+    try:
+        doi = sys.argv[1] if not doi else doi[1]
+        data = doi2tex(doi)
+        if data:
+            print(data)
+    except:
+        usage(prefix)
