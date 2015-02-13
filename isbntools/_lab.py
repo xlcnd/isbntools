f43aa6de763cf7d46660101e3c6a096d3b4082d5
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-13 11:24:13 +0000

    add write to file option to sprint

--- isbntools/_lab.py
+++ isbntools/_lab.py
@@ -11,11 +11,16 @@ PY2 = sys.version < '3'
 EOL = '\r\n' if WINDOWS and not PY2 else '\n'
 
 
-def sprint(content):
+def sprint(content, filep=None, mode='w'):
     """Smart print function so that redirection works... (see issue 75)."""
     s = content + EOL
     buf = s.encode("utf-8")
+    if filep:
+        stdout = sys.stdout
+        sys.stdout = open(filep, mode)
     if PY2:
         sys.stdout.write(buf)
     else:
         sys.stdout.buffer.write(buf)
+    if filep:
+        sys.stdout = stdout
