e226dc59e0cb54ce1ede4576065a30956a97125e
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-15 12:06:47 +0000

    license as docstring

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 # -*- coding: utf-8 -*-
+"""
+isbntools - tools for extracting, cleaning and transforming ISBNs
+Copyright (C) 2014  Alexandre Lima Conde
 
-# isbntools - tools for extracting, cleaning and transforming ISBNs
-# Copyright (C) 2014  Alexandre Lima Conde
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-
-# You should have received a copy of the GNU Lesser General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import os
 import sys
@@ -175,7 +176,7 @@ setup(
         'Environment :: Console',
         'Topic :: Text Processing :: General',
         'Topic :: Software Development :: Libraries :: Python Modules',
-         ],
+        ],
 )
 
 
