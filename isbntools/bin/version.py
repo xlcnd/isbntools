5cb302b86dc8feb2538f7e0f9ded349625aa1a00
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-16 15:57:40 +0000

    flake8

--- isbntools/bin/version.py
+++ isbntools/bin/version.py
@@ -3,12 +3,12 @@
 import time
 
 from isbnlib import RDDATE
+
 from isbntools import __version__
 from isbntools.contrib.modules.uxcolors import _colors as colors
 
 
-def main(wait=5):
-    NOTICE = """
+NOTICE = """
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Lesser General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
@@ -23,10 +23,14 @@ def main(wait=5):
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
+
+def main(wait=5):
+    """Makes an audit report."""
     print((colors.BOLD))
     print(" isbntools - tools for extracting, cleaning and transforming ISBNs")
     print((colors.RESET))
-    print((" Copyright (C) 2014  Alexandre Lima Conde, Version %s" % __version__))
+    print((" Copyright (C) 2014  Alexandre Lima Conde, Version %s" %
+          __version__))
     print("")
     print(" License LGPL v3")
     print((NOTICE))
