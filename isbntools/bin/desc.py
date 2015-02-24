ba1a57f6edb0551f065a3cb07c5ea81f1ac79b0f
Alexandre Conde <alexandreconde@hotmail.com>
2015-02-24 10:23:14 +0000

    add 'cover' and 'desc' features

---
+++ isbntools/bin/desc.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+import sys
+
+from isbntools.app import desc, quiet_errors
+
+PREFIX = 'isbn_'
+
+def usage(prefix=PREFIX):
+    print('Usage: %scover ISBN' % prefix)
+    return 1
+
+
+def main(args=None, prefix=PREFIX):
+    sys.excepthook = quiet_errors
+    try:
+        args = sys.argv[1] if not args else args[1]
+        print(desc(args))
+    except:
+        usage(prefix)
