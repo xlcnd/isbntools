00939da78e2f90019280bbc37208fbb4eccc97a1
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-15 11:40:13 +0100

    format output

--- isbntools/bin/desc.py
+++ isbntools/bin/desc.py
@@ -2,6 +2,8 @@
 
 import sys
 
+from textwrap import fill
+
 from ..app import desc, quiet_errors, uprint
 
 PREFIX = 'isbn_'
@@ -16,6 +18,9 @@ def main(args=None, prefix=PREFIX):
     sys.excepthook = quiet_errors
     try:
         args = sys.argv[1] if not args else args[1]
-        uprint(desc(args))
+        content = desc(args)
+        content = fill(content, width=75) if content else None
+        if content:
+            uprint()
     except:
         usage(prefix)
