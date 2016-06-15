d9ea54eeed88541836d750d0875191e36e54c3fc
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-15 13:29:48 +0100

    drop the support for plugins

--- isbntools/_initapp.py
+++ isbntools/_initapp.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
 """Read and set config parameters."""
 
-try:                                 # pragma: no cover
+try:  # pragma: no cover
     import configparser
-except ImportError:                  # pragma: no cover
+except ImportError:  # pragma: no cover
     import ConfigParser as configparser
 import logging
 import os
@@ -17,9 +17,7 @@ from isbnlib import registry
 
 from ._exceptions import PluginNotLoadedError
 
-
 # <--- NOTE: THIS CODE RUNS ON IMPORT! --->
-
 
 # env
 VIRTUAL = True if hasattr(sys, 'real_prefix') else False
@@ -38,18 +36,12 @@ THREADS_TIMEOUT=11
 [SERVICES]
 DEFAULT_SERVICE=merge
 VIAS_MERGE=serial
-[PLUGINS]
 [MODULES]
 """
 
 # get defaults
 SOCKETS_TIMEOUT = float(config.SOCKETS_TIMEOUT)
 THREADS_TIMEOUT = float(config.THREADS_TIMEOUT)
-
-# setup paths for contrib
-pkg_path = os.path.dirname(os.path.abspath(__file__))
-plugins_path = os.path.join(pkg_path, 'contrib/plugins')
-
 
 # set conf path
 CONF_PATH = None
@@ -61,28 +53,24 @@ def setconfpath(confpath):
     CONF_PATH = confpath
     config.set_option('CONF_PATH', confpath)
 
-
 # read/set conf file
 conf = configparser.ConfigParser()
 # read DEFAULTS (in memory)
-try:                             # pragma: no cover
-    conf.read_string(DEFAULTS)            # PY3
-except:                          # pragma: no cover
+try:  # pragma: no cover
+    conf.read_string(DEFAULTS)  # PY3
+except:  # pragma: no cover
     import io
-    conf.readfp(io.BytesIO(DEFAULTS))     # PY2
+    conf.readfp(io.BytesIO(DEFAULTS))  # PY2
 # read user options
-if VIRTUAL:                    # pragma: no cover
-    conf.files = conf.read(
-        [os.path.join(sys.prefix,
-         os.path.join('isbntools', 'isbntools.conf'))
-         ])
+if VIRTUAL:  # pragma: no cover
+    conf.files = conf.read([os.path.join(
+        sys.prefix, os.path.join('isbntools', 'isbntools.conf'))])
 else:
-    if WINDOWS:                # pragma: no cover
-        conf.files = conf.read(
-            [os.path.join(os.getenv('APPDATA'),
-                          os.path.join('isbntools', 'isbntools.conf'))
-             ])
-    else:                        # pragma: no cover
+    if WINDOWS:  # pragma: no cover
+        conf.files = conf.read([os.path.join(
+            os.getenv('APPDATA'),
+            os.path.join('isbntools', 'isbntools.conf'))])
+    else:  # pragma: no cover
         conf.files = conf.read([
             '/etc/.isbntools/isbntools.conf',
             '/usr/local/bin/isbntools.conf',
@@ -96,7 +84,6 @@ try:
 except:
     pass
 
-
 # set options
 if conf.has_section('SYS'):
     # get user defined values for timeouts
@@ -108,50 +95,19 @@ if conf.has_section('SERVICES'):
         if o.upper() == 'DEFAULT_SERVICE':
             registry.setdefaultservice(v)
             continue
-        if 'api_key' in o:       # pragma: no cover
+        if 'api_key' in o:  # pragma: no cover
             name = o[:-8]
             config.add_apikey(name, v)
         else:
             config.set_option(o.upper(), v)
 
-if conf.has_section('MISC'):     # pragma: no cover
+if conf.has_section('MISC'):  # pragma: no cover
     for o, v in conf.items('MISC'):
         config.set_option(o.upper(), v)
 
 if conf.has_section('MODULES'):  # pragma: no cover
     for o, v in conf.items('MODULES'):
         config.set_option(o.upper(), v)
-
-if conf.has_section('PLUGINS'):  # pragma: no cover
-    for o, v in conf.items('PLUGINS'):
-        path = v if '/' in v or '\\' in v else plugins_path
-        try:
-            plugin = registry.load_plugin(v, path)
-            if plugin:
-                registry.add_service(o.lower(), plugin.query)
-        except:
-            # See issue #85!
-            if v in ('isbndb.py', 'openl.py'):
-                print("*** WARNING: plugin {} is now in the core! Check {}."
-                      .format(v, 'isbntools.conf'))
-            else:
-                raise PluginNotLoadedError(v)
-
-
-# get plugins from entry_points
-try:                                 # pragma: no cover
-    for entry in iter_entry_points(group='isbntools.plugins'):
-        registry.add_service(entry.name, entry.load())
-except:                              # pragma: no cover
-    pass
-
-# get formatters from entry_points
-try:                                 # pragma: no cover
-    for entry in iter_entry_points(group='isbntools.formatters'):
-        registry.add_bibformatter(entry.name, entry.load())
-except:                              # pragma: no cover
-    pass
-
 
 # socket timeout is not exposed at urllib2 level so I had to import the
 # module and set a default value for all the sockets (timeout in seconds)
@@ -181,7 +137,6 @@ else:
         # stay with the default in-memory cache
         pass
 
-
 # set covers cache
 if config.options.get('COVERSCACHE', 'UNDEFINED').lower() == 'no':
     registry.set_covers_cache(None)
@@ -190,7 +145,6 @@ else:
     cache_path = os.path.join(CONF_PATH, CACHE_FOLDER)
     from isbnlib.dev.helpers import CoversCache
     registry.set_covers_cache(CoversCache(cache_path))
-
 
 # set logger
 fmt = "%(asctime)s;%(levelname)s;%(message)s"
@@ -203,7 +157,6 @@ if CONF_PATH:
     logging.basicConfig(filename=log_path, level=level, format=fmt)
 else:
     logging.basicConfig(level=logging.CRITICAL, format=fmt)
-
 
 # setup Windows console
 if WINDOWS:
