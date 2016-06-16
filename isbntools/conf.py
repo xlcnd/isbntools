3071301a1d5ea94947e8255beddb738f2a419265
Alexandre Conde <alexandreconde@hotmail.com>
2016-06-16 09:36:45 +0100

    delete reg_plugin

--- isbntools/conf.py
+++ isbntools/conf.py
@@ -2,8 +2,7 @@
 """Edit conf file (mainly for programatic API)."""
 
 from ._conf import (conf_file, mk_conf, pkg_options, pkg_path, pkg_version,
-                    print_conf, reg_apikey, reg_mod, reg_myopt, reg_plugin)
+                    print_conf, reg_apikey, reg_mod, reg_myopt)
 
-__all__ = ('pkg_version', 'pkg_path', 'pkg_options',
-           'reg_mod', 'reg_plugin', 'reg_apikey', 'reg_myopt',
-           'conf_file', 'mk_conf', 'print_conf')
+__all__ = ('pkg_version', 'pkg_path', 'pkg_options', 'reg_mod', 'reg_apikey',
+           'reg_myopt', 'conf_file', 'mk_conf', 'print_conf')
