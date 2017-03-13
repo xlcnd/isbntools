d94f113bcbfe365658d8ca0a34b5f783200ea789
Alexandre Conde <alexandreconde@hotmail.com>
2017-03-13 16:57:55 +0000

    qa - typo

--- isbntools/contrib/modules/report/_audit.py
+++ isbntools/contrib/modules/report/_audit.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-
 """Performs an audit and reports on installed scripts and plugins."""
 
 from pkg_resources import iter_entry_points
@@ -11,18 +10,21 @@ def audit():
     """Report on installed scripts and plugins."""
     errcode = 1
 
-    cmds = [entry.name for entry in iter_entry_points(group='console_scripts')
-            if 'isbn' in entry.name]
+    cmds = [
+        entry.name for entry in iter_entry_points(group='console_scripts')
+        if 'isbn' in entry.name
+    ]
     if cmds:  # pragma: no cover
-        print(' The following isbn commands are available on your system:')
+        print(' The following isbn commands are available in your system:')
         print('')
         columnize(sorted(cmds))
         errcode = 0
 
-    plug = [entry.name for entry in
-            iter_entry_points(group='isbntools.plugins')]
+    plug = [
+        entry.name for entry in iter_entry_points(group='isbntools.plugins')
+    ]
     if plug:  # pragma: no cover
-        print(' The following isbntools plugins are available on your system:')
+        print(' The following isbntools plugins are available in your system:')
         print('')
         columnize(sorted(plug))
         errcode = 1 if errcode == 1 else 0
