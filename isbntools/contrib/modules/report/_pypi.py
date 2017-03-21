4338201565689a616253303f310d0bb1bd092b08
Alexandre Conde <alexandreconde@hotmail.com>
2017-03-21 11:20:18 +0000

    fix VIRTUAL to work with pyvenv in PY3

--- isbntools/contrib/modules/report/_pypi.py
+++ isbntools/contrib/modules/report/_pypi.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 # pylint: skip-file
-
 """Check available packages in pypi."""
 
 import os
@@ -10,12 +9,15 @@ from subprocess import PIPE, Popen
 
 from isbntools.contrib.modules.uxcolors import _colors as colors
 
-
 PY2 = sys.version < '3'
 PKGS = ('isbntools', 'isbnlib')
 WINDOWS = os.name == 'nt'
 EOL = '\r\n' if WINDOWS and not PY2 else '\n'
+PY3 = not PY2
 VIRTUAL = True if hasattr(sys, 'real_prefix') else False
+VENV = True if hasattr(sys, 'sys.base_prefix') else False
+if not VIRTUAL and VENV and PY3:  # pragma: no cover
+    VIRTUAL = sys.prefix != sys.base_prefix  # inside pyvenv environement?
 BOLD = colors.BOLD
 RESET = colors.RESET
 
@@ -24,20 +26,20 @@ def shell(shcmd=None):
     """Run a shell command."""
     if not shcmd:  # pragma: no cover
         return
-    sp = Popen(shcmd,
-               shell=True,
-               stdin=PIPE,
-               stdout=PIPE,
-               stderr=PIPE,
-               close_fds=not WINDOWS
-               )
+    sp = Popen(
+        shcmd,
+        shell=True,
+        stdin=PIPE,
+        stdout=PIPE,
+        stderr=PIPE,
+        close_fds=not WINDOWS)
     (fo, fe) = (sp.stdout, sp.stderr)
     if PY2:  # pragma: no cover
-       out = fo.read().strip(EOL)
-       err = fe.read().strip(EOL)
+        out = fo.read().strip(EOL)
+        err = fe.read().strip(EOL)
     else:  # pragma: no cover
-       out = fo.read().decode("utf-8")
-       err = fe.read().decode("utf-8")
+        out = fo.read().decode("utf-8")
+        err = fe.read().decode("utf-8")
     if out:
         return out
     if err:  # pragma: no cover
@@ -54,9 +56,10 @@ def check_pypi(pkgs=PKGS):
         out = shell(cmd + ' '.join(pkgs))
         if out == '1':  # pragma: no cover
             return 1
-        if out:         # pragma: no cover
+        if out:  # pragma: no cover
             print('')
-            print(' At %spypi%s, the following packages are available:' % (BOLD, RESET))
+            print(' At %spypi%s, the following packages are available:' %
+                  (BOLD, RESET))
             print('')
             print(out)
         return 0
