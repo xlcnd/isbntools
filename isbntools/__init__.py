dfee2366cade6e682af00b1ffd0ebf7b157f0d25
Alexandre Conde <alexandreconde@hotmail.com>
2017-06-19 11:43:05 +0100

    init v4.3.6

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 """Define isbntools version and support IDs."""
 
-__version__ = '4.3.5'
+__version__ = '4.3.6'
 __support__ = 'py26, py27, py32, py33, py34, py35, pypy, pypy3'
