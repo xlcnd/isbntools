f6edf6116345206440e296b7c01847bd9949aa47
Alexandre Conde <alexandreconde@hotmail.com>
2017-06-27 18:01:35 +0100

    close #91

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -2,4 +2,4 @@
 """Define isbntools version and support IDs."""
 
 __version__ = '4.3.6'
-__support__ = 'py26, py27, py32, py33, py34, py35, pypy, pypy3'
+__support__ = 'py26, py27, py32, py33, py34, py35, py36, pypy, pypy3'
--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ SECONDRUN = INSTALL and not FIRSTRUN
 
 # CHECK SUPPORT
 if INSTALL and FIRSTRUN:
-    SUPPORTED = ((2, 6), (2, 7), (3, 2), (3, 3), (3, 4), (3, 5))
+    SUPPORTED = ((2, 6), (2, 7), (3, 2), (3, 3), (3, 4), (3, 5), (3, 6))
     if tuple(int(x) for x in sys.version[:3].split('.')) not in SUPPORTED:
         raise Exception('isbntools %s  requires Python 2.6+ or 3.3+.' %
                         __version__)
