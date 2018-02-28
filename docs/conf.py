8f89104ab28217cd9db3a8dd4a33840800444a89
Alexandre Conde <alexandreconde@hotmail.com>
2018-02-28 12:21:58 +0000

    normalize Copyright

--- docs/conf.py
+++ docs/conf.py
@@ -45,7 +45,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'isbntools'
-copyright = u'2016, Alexandre Conde'
+copyright = u'2014-2018, Alexandre Conde'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 
 """
 isbntools - extract, transform and metadata for ISBNs
-Copyright (C) 2018  Alexandre Lima Conde
+Copyright (C) 2014-2018  Alexandre Lima Conde
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
