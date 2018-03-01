2448df5095b85e4600d533ae9e16303d35a15792
Alexandre Conde <alexandreconde@hotmail.com>
2018-03-01 16:39:44 +0000

    add copyright notice

--- isbntools/_shelvecache.py
+++ isbntools/_shelvecache.py
@@ -13,6 +13,24 @@ NOTES
 5. The cache is optimized for low hit frequency (using a simple dict lookup
    not a Bloom filter!).
 
+
+This file belongs to the project:
+
+isbntools - extract, transform and metadata for ISBNs
+Copyright (C) 2014-2018  Alexandre Lima Conde
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 # TODO rewrite using context managers!
