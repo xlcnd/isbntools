74715129410a9826fd7b3a48dd8920c981a044e9
Alexandre Conde <alexandreconde@hotmail.com>
2019-02-26 08:31:37 +0000

    add speed test

---
+++ isbntools/test/speed.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+"""Crude Timer for 'import speed'."""
+
+import time
+
+
+print("Test 'import speed' of:")
+
+
+def speed_initapp():
+    """Test import speed of '_initapp'."""
+    t = time.process_time()
+    import isbntools._initapp
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print("(_initapp)  {} milliseconds < 200 milliseconds".format(millis))
+    assert millis < 200
+
+def speed_app():
+    """Test import speed of 'app'."""
+    t = time.process_time()
+    import isbntools.app
+    elapsed_time = time.process_time() - t
+    millis = int(elapsed_time * 1000)
+    print("(app)         {} milliseconds <  10 milliseconds".format(millis))
+    assert millis < 10
+
+
+
+speed_initapp()
+speed_app()
