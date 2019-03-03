106e018fef9e763741ec8f09ebc0138c5025e9be
Alexandre Conde <alexandreconde@hotmail.com>
2019-03-03 10:53:51 +0000

    (WIP) add 'merge' service

---
+++ isbntools/_merge.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""Provide metadata by merging metadata from other providers."""
+
+from . import config
+from . import goob as qgoob
+from . import oclc as qoclc
+from isbnlib.dev import Metadata, vias
+
+
+# TODO register new service 'merge' on import
+
+
+def query(isbn, processor=None):
+    """Query function for the 'merge provider' (waterfall model)."""
+    if not processor:
+        processor = config.options.get('VIAS_MERGE', processor).lower()
+        if not processor:     # pragma: no cover
+            processor = 'serial'
+
+    named_tasks = (('oclc', qoclc), ('goob', qgoob))
+    if processor == 'parallel':
+        results = vias.parallel(named_tasks, isbn)
+    elif processor == 'serial':
+        results = vias.serial(named_tasks, isbn)
+    elif processor == 'multi':
+        results = vias.multi(named_tasks, isbn)
+
+    rw = results.get('oclc')
+    rg = results.get('goob')
+
+    if not rw and not rg:
+        return None
+
+    md = Metadata(rw) if rw else None
+
+    if md and rg:
+        # Overwrite with Authors and Language from Google
+        md.merge(rg, overwrite=('Authors'))
+        return md.value
+    if not md and rg:       # pragma: no cover
+        md = Metadata(rg)
+        return md.value
+    return md.value if not rg and rw else None  # pragma: no cover
