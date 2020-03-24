a9cfc9175734d570e166cd56062559d886d65da6
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-24 14:42:50 +0000

    better 'classify' output

--- isbntools/bin/classify.py
+++ isbntools/bin/classify.py
@@ -2,6 +2,7 @@
 
 import sys
 
+from json import dumps
 from ..app import classify, quiet_errors
 
 PREFIX = 'isbn_'
@@ -16,6 +17,10 @@ def main(isbn=None, prefix=PREFIX):
     sys.excepthook = quiet_errors
     try:
         isbn = sys.argv[1] if not isbn else isbn[1]
-        print(classify(isbn))
+        data = classify(isbn)
+        if data:
+            print(dumps(data, indent = 4))
+        else:
+            raise Exception("No data")
     except:
         usage(prefix)
