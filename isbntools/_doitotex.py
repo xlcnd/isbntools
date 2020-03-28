d15700f2714d95e3f125993b871439d32bd6730b
Alexandre Conde <alexandreconde@hotmail.com>
2020-03-28 11:37:34 +0000

    add local 'doi2tex'

---
+++ isbntools/_doitotex.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+"""Return metadata, of a given DOI, formated as BibTeX."""
+
+import logging
+
+from isbnlib.dev import cache
+from isbnlib.dev.webservice import query
+
+LOGGER = logging.getLogger(__name__)
+
+URL = 'http://dx.doi.org/{DOI}'
+UA = 'isbnlib (gzip)'
+
+
+@cache
+def doi2tex(doi):
+    """Get the bibtex ref for doi."""
+    data = query(
+        URL.format(DOI=doi),
+        user_agent=UA,
+        appheaders={
+            'Accept': 'application/x-bibtex; charset=utf-8',
+        },
+    )  # noqa  pragma: no cover
+    if not data:  # pragma: no cover
+        LOGGER.warning('no data return for doi: %s', doi)
+    return data if data else None  # pragma: no cover
---
+++ isbntools/test/test_doi2tex.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# flake8: noqa
+# pylint: skip-file
+"""nose tests for doi2tex."""
+
+from nose.tools import assert_equals
+
+from ..app import doi2tex
+
+
+def test_doi2tex():
+    """Test the doi2tex service."""
+    assert_equals(len(repr(doi2tex('10.2139/ssrn.2411669'))) > 50, True)
