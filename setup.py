19cd01392c7aa7aacdc6f71f3ad7691d0f7bc2cd
Alexandre Conde <alexandreconde@hotmail.com>
2020-12-31 11:09:14 +0000

    update year

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 # flake8: noqa
 """
 isbntools - extract, transform and metadata for ISBNs
-Copyright (C) 2014-2020  Alexandre Lima Conde
+Copyright (C) 2014-2021  Alexandre Lima Conde
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU Lesser General Public License as published by
