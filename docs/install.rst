658e36df9ee44f2c4e7f1b8a6e8a84fe32a31340
Alexandre Conde <alexandreconde@hotmail.com>
2021-04-13 12:35:31 +0100

    init v4.3.28

--- docs/install.rst
+++ docs/install.rst
@@ -15,7 +15,7 @@ or:
 
 .. code-block:: bash
 
-    $ pip install isbntools-4.3.27.tar.gz
+    $ pip install isbntools-4.3.28.tar.gz
 
 (first you have to download the file!)
 
--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
 """Define isbntools version and support IDs."""
 
-__version__ = '4.3.27'
+__version__ = '4.3.28'
 __support__ = 'py27, py35, py36, py37, py38, py39'
