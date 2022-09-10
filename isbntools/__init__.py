329493e84ef643e26b424e9f1e1671694b3a4bda
Alexandre Conde <alexandreconde@hotmail.com>
2022-09-10 22:00:13 +0100

    add support for python 3.10

--- isbntools/__init__.py
+++ isbntools/__init__.py
@@ -2,4 +2,4 @@
 """Define isbntools version and support IDs."""
 
 __version__ = '4.3.29'
-__support__ = 'py27, py35, py36, py37, py38, py39'
+__support__ = 'py27, py35, py36, py37, py38, py39, py310'
